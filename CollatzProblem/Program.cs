3c8683b8e6d6da49042c589e47ae2b8de86d6d86
Gvidas Sniolis <gsniolis@gmail.com>
2020-04-27 22:16:59 +0300

    Experimenting with parallelism and Collatz conjecture

--- CollatzProblem/Program.cs
+++ CollatzProblem/Program.cs
@@ -1,43 +1,75 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CollatzProblem
 {
     class Program
     {
+        static long maxSteps = 0;
+        static object _lock = new object();
+        static readonly Stopwatch t = new Stopwatch();
+
         static void Main(string[] args)
         {
-            Collatz(new long[5] { 1477, 1311239111, 7, 3, 19 });
+            int numberOfThreads = 16;
+            long[] numbers = new long[100000];
+            InitData(ref numbers);
+
+            if (args.Length == 0)
+                Console.WriteLine("Number of threads to use was not supplied. 4 Threads will be used");
+            else
+                numberOfThreads = int.Parse(args[0]);
+
+            t.Start();
+            /*foreach (long number in numbers)
+                Collatz(number);*/
+
+            ParallelOptions po = new ParallelOptions();
+            po.MaxDegreeOfParallelism = numberOfThreads;
+
+            Parallel.ForEach(numbers, po, (currentNumber) =>
+            {
+                Collatz(currentNumber);
+            });
+            t.Stop();
+
+            Console.WriteLine("Max steps: {0}", maxSteps);
+            Console.WriteLine("Executed in {0}ms", t.ElapsedMilliseconds);
+
         }
 
-        static void Collatz(long[] numbers)
+        public static void Collatz(long number)
         {
-            long currentNumber = 0;
-            
-            long maxSteps = 0;
-            
-            foreach(long number in numbers)
+            long steps = 0;
+            long originalNumber = number;
+
+            while (number != 1)
             {
-                Console.WriteLine("Collatz() with {0}", number);
-                currentNumber = number;
-                long steps = 0;
-                while (currentNumber != 1)
+                if (number % 2 != 0)
                 {
-                    if (currentNumber % 2 != 0)
-                    {
-                        currentNumber = 3 * currentNumber + 1;
-                    } else
-                    {
-                        currentNumber = currentNumber / 2;
-                    }
-                    Console.Write($"{currentNumber} ");
-                    steps++;
-
+                    number = 3 * number + 1;
+                } else
+                {
+                    number = number / 2;
                 }
-                maxSteps = (steps >= maxSteps) ? steps : maxSteps;
-                Console.WriteLine();
-            }
+                steps++;
 
-            Console.WriteLine("Max iterations: {0}", maxSteps);
+            }
+            //Console.WriteLine("[{0}]Collatz({1}) steps : {2}     Duration: {3}ms", Thread.CurrentThread.ManagedThreadId, originalNumber, steps, t.ElapsedMilliseconds);
+
+            lock(_lock)
+            {
+                maxSteps = (maxSteps <= steps) ? steps : maxSteps;
+            }
+        }
+
+        public static void InitData(ref long[] numbers)
+        {
+            Random r = new Random(int.MaxValue);
+            for (int i = 0; i < 100000; i++)
+                numbers[i] = r.Next();
         }
     }
 }
